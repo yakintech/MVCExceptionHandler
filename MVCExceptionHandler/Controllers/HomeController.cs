0af60d4bed5c55a421f86d2bfb9e0a5bbbc947d2
Çağatay Yıldız <cagatay.yildiz@yakintech.com>
2017-07-27 01:01:28 +0300

    Add project files.

---
+++ MVCExceptionHandler/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using MVCExceptionHandler.Models.Attributes.ExceptionAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExceptionHandler.Controllers
+{
+
+    public class HomeController : Controller
+    {
+
+        //[HandleError(ExceptionType = typeof(IndexOutOfRangeException), View = "DatabaseError")]
+       // [MyErrorHandle]
+        public ActionResult Index()
+        {
+           // throw new Exception();
+            
+            return View();
+        }
+
+        public ActionResult DatabaseError()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCExceptionHandler/Models/Attributes/ExceptionAttributes/MyErrorHandle.cs
@@ -0,0 +1,35 @@
+﻿using MVCExceptionHandler.Models.ORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExceptionHandler.Models.Attributes.ExceptionAttributes
+{
+    public class MyErrorHandle : HandleErrorAttribute, IExceptionFilter
+    {
+
+
+        
+        public override void OnException(ExceptionContext filterContext)
+        {
+            filterContext.ExceptionHandled = true;
+
+            var exMessage = filterContext.Exception.Message;
+            var controllerName = filterContext.RouteData.Values["controller"];
+            var actionName = filterContext.RouteData.Values["action"];
+            var message = String.Format("Message:{0} controller:{1} action:{2}", exMessage, controllerName, actionName);
+
+
+
+            filterContext.ExceptionHandled = true;
+
+
+            filterContext.Result = new ViewResult
+            {
+                ViewName = "~/Views/Error/Index.cshtml"
+            };
+        }
+    }
+}
